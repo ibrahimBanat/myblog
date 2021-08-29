c7b6a8545427069a5ee9207ce4a62e17cb748592
IbrahimBanat <ibrahim.banat.97@gmail.com>
2021-08-29 17:11:52 +0300

    creating config php and connect to the db

--- link/sitemap.php
+++ link/sitemap.php
@@ -1,3 +1,48 @@
 <?php
 ob_start();
 session_start();
+
+//Database Details
+
+define('DBHOST', 'localhost');
+define('DBUSER', 'ibrahim');
+define('DBPASS', '1234');
+define('DBNAME', 'blog');
+
+
+$db = new PDO("mysql:host=".DBHOST."dbname=".DBNAME, DBUSER, DBPASS);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+//Set the time zone
+date_default_timezone_set('africa/cairo');
+
+
+//Auto load classes
+
+function __autoload($class){
+    $class = strtolower($class);
+
+    //if call from within assets adjust the path
+
+    $classPath = 'classes/class.'.$class . '.php';
+    if(file_exists($classPath)) {
+        require_once $classPath;
+    }
+    //if the call within the assets adjust the path
+
+    $classPath = '../classes/class.'. $class . '.php';
+    if (file_exists($classPath)) {
+        require_once $classPath;
+    }
+    //if the call from within admin adjust the path
+
+    $classPath = '../../classes/class'. $class . '.php';
+
+    if(file_exists($filePath)) {
+        require_once $filePath;
+    }
+
+
+}
+
+$user = new User($db);
